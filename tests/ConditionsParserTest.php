56a2cdcb855b06c311b61c2cdb35a413e934410c
cinam <cinam@hotmail.com>
2017-11-25 14:00:01 +0100

    Added test for two ifs.

--- tests/ConditionsParserTest.php
+++ tests/ConditionsParserTest.php
@@ -37,5 +37,20 @@ class ConditionsParserTest extends \PHPUnit\Framework\TestCase
         $text2 = 'begin [IF 0]two[ENDIF] end';
         $this->assertEquals('begin  end', $this->parser->parse($text2));
     }
+
+    public function testTwoIfs()
+    {
+        $text = 'begin [IF 1]one[ENDIF] middle [IF 1]two[ENDIF] end';
+        $this->assertEquals('begin one middle two end', $this->parser->parse($text));
+
+        $text = 'begin [IF 1]one[ENDIF] middle [IF 0]two[ENDIF] end';
+        $this->assertEquals('begin one middle  end', $this->parser->parse($text));
+
+        $text = 'begin [IF 0]one[ENDIF] middle [IF 1]two[ENDIF] end';
+        $this->assertEquals('begin  middle two end', $this->parser->parse($text));
+    
+        $text = 'begin [IF 0]one[ENDIF] middle [IF 0]two[ENDIF] end';
+        $this->assertEquals('begin  middle  end', $this->parser->parse($text));
+    }
 }
 
