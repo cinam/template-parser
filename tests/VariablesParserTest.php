aef531a146bf8691964c30abb0236a95fe647c48
cinam <cinam@hotmail.com>
2017-11-28 05:52:11 +0100

    Tests for standard variables parsing.

---
+++ tests/VariablesParserTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Cinam\TemplateParser\Tests;
+
+use Cinam\TemplateParser\VariablesParser;
+
+class VariablesParserTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @var VariablesParser
+     */
+    protected $parser;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->parser = new VariablesParser();
+    }
+
+    public function tearDown()
+    {
+        unset($this->parser);
+    }
+
+    public function testNoVariables()
+    {
+        $text = 'begin middle end';
+        $this->assertEquals('begin middle end', $this->parser->parse($text));
+    }
+
+    /**
+     * @dataProvider providerStandard
+     */
+    public function testStandard($input, $variables, $expected)
+    {
+        $this->assertEquals($expected, $this->parser->parse($input, $variables));
+    }
+
+    public function providerStandard()
+    {
+        return [
+            ['begin {var1} end', ['var1' => 1], 'begin 1 end'],
+            ['begin {var1} {var1} end', ['var1' => 1], 'begin 1 1 end'],
+            ['begin {var1} {var2} end', ['var1' => 1, 'var2' => 2], 'begin 1 2 end'],
+        ];
+    }
+}
+
